2035d892efe586bf0d54841c590ed50965d21f1c
jinke <1359518268@qq.com>
2017-11-30 16:42:26 +0800

    fuck

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry  } from 'react-native'
-import App from './src/index'
+import App from './App'
 
 AppRegistry.registerComponent('rnLijinkeWeb', () => App)
-AppRegistry.runApplication('rnLijinkeWeb', { rootTag: document.getElementById('react-root') })
+// AppRegistry.runApplication('rnLijinkeWeb', { rootTag: document.getElementById('react-root') })
